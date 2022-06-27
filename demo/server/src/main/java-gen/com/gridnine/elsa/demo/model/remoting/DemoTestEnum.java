ecfe2a7b4881bc9f02f063c352d579367367baa9
Vladimir Avramenko <vladimir.avramenko@gridnine.com>
2022-06-27 16:24:31 +0300

    rest

---
+++ demo/server/src/main/java-gen/com/gridnine/elsa/demo/model/remoting/DemoTestEnum.java
@@ -0,0 +1,12 @@
+/*****************************************************************
+ * This is generated code, don't modify it manually
+ *****************************************************************/
+
+package com.gridnine.elsa.demo.model.remoting;
+
+public enum DemoTestEnum{
+
+	ITEM1,
+
+	ITEM2
+}
---
+++ demo/site/src-gen/demo-test-types.ts
@@ -0,0 +1,23 @@
+/* ****************************************************************
+ * This is generated code, don't modify it manually
+ **************************************************************** */
+
+export type DemoTestEnum=
+'ITEM1'
+| 'ITEM2';
+
+export type DemoTestServerCallRequest={
+  param?: string,
+};
+
+export type DemoTestServerCallResponse={
+  stringProperty?: string,
+};
+
+export type DemoTestSubscriptionParameters={
+  param?: string,
+};
+
+export type DemoTestSubscriptionEvent={
+  stringProperty?: string,
+};
