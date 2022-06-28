5010a12b3ffefe3f1d57936f52c668f465e04879
Vladimir Avramenko <vladimir.avramenko@gridnine.com>
2022-06-28 21:14:10 +0300

    rest

---
+++ demo/server/src/main/java-gen/com/gridnine/elsa/demo/model/remoting/DemoTestEntity.java
@@ -0,0 +1,41 @@
+/*****************************************************************
+ * This is generated code, don't modify it manually
+ *****************************************************************/
+
+package com.gridnine.elsa.demo.model.remoting;
+
+import com.gridnine.elsa.common.core.model.common.BaseIntrospectableObject;
+
+public class DemoTestEntity extends BaseIntrospectableObject{
+
+	private String stringProperty;
+
+	public String getStringProperty(){
+		return stringProperty;
+	}
+
+	public void setStringProperty(String value){
+		this.stringProperty = value;
+	}
+
+	@Override
+	public Object getValue(String propertyName){
+
+		if("stringProperty".equals(propertyName)){
+			return stringProperty;
+		}
+
+		return super.getValue(propertyName);
+	}
+
+	@Override
+	public void setValue(String propertyName, Object value){
+
+		if("stringProperty".equals(propertyName)){
+			this.stringProperty = (String) value;
+			return;
+		}
+
+		super.setValue(propertyName, value);
+	}
+}
