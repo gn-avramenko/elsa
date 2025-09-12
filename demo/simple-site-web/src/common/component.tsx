e6071e83c441724e63279a3aa6c5189b955c28e8
Vladimir Avramenko <vladimir.avramenko@gridnine.com>
2025-09-12 21:50:38 +0300

    general web codegen

---
+++ demo/simple-site-web/src/common/component.tsx
@@ -0,0 +1,142 @@
+import React, { ReactElement } from 'react';
+import {
+    BaseUiElement,
+    PreloaderMiddleware,
+    webpeerExt,
+    WebPeerExtension,
+} from 'webpeer-core';
+import { createRoot, Root } from 'react-dom/client';
+
+export interface ReactUiElementFactory {
+    createElement(model: any): BaseReactUiElement;
+}
+
+export type ReactWebPeerExtension = WebPeerExtension & {
+    elementHandlersFactories: Map<string, ReactUiElementFactory>;
+};
+export const reactWebPeerExt = webpeerExt as ReactWebPeerExtension;
+
+reactWebPeerExt.elementHandlersFactories = new Map();
+
+export type InputType = 'TEXT_FIELD' | 'SELECT';
+
+export type FlexDirection = 'ROW' | 'COLUMN';
+
+export type ReactElementDescription = {
+    state: string[];
+    actionsFromServer: string[];
+    actionsFromClient: string[];
+};
+export abstract class BaseReactUiElement extends BaseUiElement {
+    readonly description: ReactElementDescription;
+
+    state = new Map<string, any | null | undefined>();
+
+    counter = 1;
+
+    stateSetters: Map<string, (value?: any) => void> = new Map<
+        string,
+        (value?: any) => void
+    >();
+
+    createReactElement(): React.ReactElement {
+        return React.createElement(this.functionalComponent, {
+            element: this,
+            key: this.id,
+        });
+    }
+
+    abstract functionalComponent: (props: any) => ReactElement;
+
+    protected constructor(description: ReactElementDescription, model: any) {
+        super(model);
+        this.description = description;
+        (model.children || []).forEach((ch: any) => {
+            const elm = reactWebPeerExt.elementHandlersFactories
+                .get(ch.type)!
+                .createElement(ch);
+            elm.parent = this;
+            elm.init();
+            this.children = this.children || [];
+            this.children.push(elm);
+        });
+        this.state.set('counter', this.counter);
+    }
+
+    protected findByTag(tag: string) {
+        return this.children?.find((it) => it.tag === tag) as BaseReactUiElement;
+    }
+
+    getState(): any {
+        const result = super.getState();
+        this.state.forEach((value, key) => {
+            result[key] = value;
+        });
+        return result;
+    }
+
+    redraw() {
+        this.counter++;
+        this.stateSetters.get('counter')!(this.counter);
+    }
+
+    processCommandFromServer(commandId: string, data?: any) {
+        if (commandId === 'pc') {
+            this.updatePropertyValue(data!.pn, data!.pv);
+            return;
+        }
+        super.processCommandFromServer(commandId, data);
+    }
+
+    protected updatePropertyValue(pn: string, pv: any) {
+        this.state.set(pn, pv);
+        this.stateSetters.get(pn)?.call(this, pv);
+    }
+}
+
+export const registerFactory = (type: string, factory: ReactUiElementFactory) => {
+    webpeerExt.elementTypes = webpeerExt.elementTypes || [];
+    webpeerExt.elementTypes.push(type);
+    reactWebPeerExt.elementHandlersFactories.set(type, factory);
+};
+
+let root: Root | null = null;
+
+export const preloaderHolder = {
+    showPreloader: () => {},
+    hidePreloader: () => {},
+};
+
+reactWebPeerExt.setMiddleware([
+    new PreloaderMiddleware(
+        {
+            showPreloader() {
+                preloaderHolder.showPreloader();
+            },
+            hidePreloader() {
+                preloaderHolder.hidePreloader();
+            },
+        },
+        {
+            delay: 300,
+        }
+    ),
+]);
+reactWebPeerExt.uiHandler = {
+    drawUi(rootElm: BaseReactUiElement) {
+        if (!root) {
+            root = createRoot(document.getElementById('root') as Element);
+        }
+        root.render(rootElm.createReactElement());
+    },
+    createElement(model: any): BaseUiElement {
+        return reactWebPeerExt.elementHandlersFactories
+            .get(model.type)!
+            .createElement(model);
+    },
+    handleServerUpdate() {
+        if (confirm('Server was updated, reload?')) {
+            window.location.reload();
+        }
+    },
+};
