7dc76b61e46af3548d7cb0406f8d17bc4c23a758
Vladimir Avramenko <vladimir.avramenko@gridnine.com>
2025-11-18 14:17:43 +0300

    login

--- platform/server-admin/src/main/java/com/gridnine/platform/elsa/admin/utils/HttpUtils.java
+++ platform/server-admin/src/main/java/com/gridnine/platform/elsa/admin/utils/HttpUtils.java
@@ -2,10 +2,16 @@ package com.gridnine.platform.elsa.admin.utils;
 
 import com.gridnine.platform.elsa.common.core.utils.TextUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 
 public class HttpUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(HttpUtils.class);
     public static String getQueryParameter(HttpServletRequest request, String name) {
         var queryString = request.getQueryString();
         if(TextUtils.isBlank(queryString)) {
@@ -20,5 +26,33 @@ public class HttpUtils {
             }
         }
         return null;
+    }
+
+    public static void disableSSLVerification() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = (hostname, session) -> true;
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (Exception e) {;
+            logger.error("unable to disable ssl verification", e);
+        }
     }
 }
